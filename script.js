58b895d547462e083905ee4266d67002b45505e3
sdc_a <sdc_ac@mail.ru>
2024-01-14 23:31:42 +0300

    lab4

--- script.js
+++ script.js
@@ -10,32 +10,11 @@ function measurePageLoadTime() {
 }
 
 window.addEventListener("load", measurePageLoadTime);
-
-document.addEventListener("DOMContentLoaded", function () {
-    const navButtons = document.querySelectorAll(".nav-buttons");
-
-    for (let i = 0; i < navButtons.length; i++) {
-        navButtons[i].addEventListener("click", function (event) {
-            event.preventDefault();
-
-            // Убираем подчеркивание у всех элементов
-            for (let j = 0; j < navButtons.length; j++) {
-                navButtons[j].classList.remove("clicked");
-            }
-
-            // Добавляем подчеркивание только для текущего элемента
-            this.classList.add("clicked");
-
-            // Получаем ссылку из атрибута href
-            const targetPage = this.getAttribute("href");
-
-            // Проверяем, находимся ли мы уже на этой странице
-            if (targetPage && document.location.pathname === targetPage) {
-                this.classList.add("clicked");
-            } else {
-                // Иначе переходим на новую страницу
-                document.location.href = targetPage;
-            }
-        });
+document.addEventListener("DOMContentLoaded", function() {console.log(1)
+    let navbar = document.querySelector('.nav-bar').children;
+    for (let i = 0; i < navbar.length; i++) {
+        if (document.location["href"] === navbar[i].children[0].href) {
+           navbar[i].children[0].classList.add('active-state');
+        }
     }
 });
