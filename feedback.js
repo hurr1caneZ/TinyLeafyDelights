665a450fc284448c098d7dbc4ccdfac7a27780e7
sdc_a <sdc_ac@mail.ru>
2024-02-06 21:32:16 +0300

    lab-7

--- feedback.js
+++ feedback.js
@@ -5,16 +5,23 @@ feedbackForm.addEventListener("submit", (e) => {
     if (validationFeedback(feedback)){
         let newFeedback = createFeedback(feedback);
         document.querySelector(".feedbacks-list").appendChild(newFeedback);
-        let feedbackList = JSON.parse(localStorage.getItem("feedbacks")) || [];
+        let feedbackList = JSON.parse(localStorage.getItem("feedbacks") || '[]');
         feedbackList.push(feedback);
         localStorage.setItem("feedbacks", JSON.stringify(feedbackList));
+        Toastify({
+            text: "Отзыв успешно добавлен", // текст внутри уведомления
+            close: true, // возможность закрытия уведомления
+            style: { //цвет уведомления
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGNLdW8xsD33A--RJ0CnGNOTE_3t6aSwB_4GuJcUn1PA&s' //картинка перед текстом уведомления
+        }).showToast();
     }
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-    let feedbackList = JSON.parse(localStorage.getItem("feedbacks")) || [];
+    let feedbackList = JSON.parse(localStorage.getItem("feedbacks") || '[]');
     let el = document.querySelector(".feedbacks-list");
-    console.log(feedbackList);
     for (let i = 0; i < feedbackList.length; i++) {
         el.appendChild(createFeedback(feedbackList[i]));
     }
@@ -30,6 +37,5 @@ function createFeedback(feedback) {
     clone.children[0].textContent = feedback;
     return clone;
 }
-
 
 
